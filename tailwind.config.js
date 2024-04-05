4c1e559f103199b9289fd8550c3ff1b0477f7db7
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-04-05 09:32:09 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    letterSpacing: {
+      space: "0.35em",
+    },
+    extend: {
+      boxShadow: {
+        shadowBtn: "0 0 30px",
+      },
+      colors: {
+        lightCyan: "hsl(193, 38%, 86%)",
+        neonGreen: "hsl(150, 100%, 66%)",
+        grayishBlue: "hsl(217, 19%, 38%)",
+        darkGrayishBlue: "hsl(217, 19%, 24%)",
+        darkBlue: "hsl(218, 23%, 16%)",
+      },
+    },
+  },
+  plugins: [],
+};
